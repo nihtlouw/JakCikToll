eacc8108fc5679aed62eb85d1246b5fbadfcae7f
nihtlouw <hirumiminazuki360@gmail.com>
2024-08-29 23:41:48 +0700

    3rd try project configuration

---
+++ JakCIkToll/app/src/main/java/com/manpro/jakciktoll/ui/Detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.manpro.jakciktoll.ui.Detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.manpro.jakciktoll.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
---
+++ JakCIkToll/app/src/main/java/com/manpro/jakciktoll/ui/Splashscreen/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.manpro.jakciktoll.ui.Splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.manpro.jakciktoll.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
